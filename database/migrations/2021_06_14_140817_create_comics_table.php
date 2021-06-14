5d732751294b05e8a80a95d5a09b7e6ba182bb4f
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-14 16:47:40 +0200

    add: seeds migrate

--- database/migrations/2021_06_14_140817_create_comics_table.php
+++ database/migrations/2021_06_14_140817_create_comics_table.php
@@ -15,6 +15,12 @@ class CreateComicsTable extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 70);
+            $table->string('slug');
+            $table->text('description');
+            $table->float('price', 4,2)->unsigned();            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 60);
             $table->timestamps();
         });
     }
