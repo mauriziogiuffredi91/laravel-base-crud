0f89022b70033e35fcd98c20633f48a00f3c77bd
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-14 15:48:01 +0200

    style and home

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel Crud </title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+
+        
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+       
+    </head>
+    <body>
+
+        <header></header>
+
+        <main>
+            <section>
+
+                
+
+            </section>
+
+        </main>
+
+        <footer></footer>
+        
+    </body>
+</html>
