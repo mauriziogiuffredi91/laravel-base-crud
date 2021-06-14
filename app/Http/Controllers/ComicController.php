76527ae01407aba941d7baa57cde43ef4226660d
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-14 18:49:36 +0200

    add:index archivio

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -14,7 +14,9 @@ class ComicController extends Controller
      */
     public function index()
     {
-        return view('comics.index');
+        $comics = Comic::all();
+        dump($comics);
+        return view('comics.index', compact('comics'));
     }
 
     /**
