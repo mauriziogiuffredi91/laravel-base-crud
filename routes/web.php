4457a3aa54942b2f3b772901773176522d9662de
mauriziogiuffredi91 <giuffro91@gmail.com>
2021-06-14 16:12:55 +0200

    add: model and migration

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('homepage');
-})->name('home');
+Route::get('/', 'PageController@index')->name('home');
